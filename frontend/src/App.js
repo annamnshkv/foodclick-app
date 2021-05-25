45034e6d9140a5c80dc6846cd1c0d3ac5f4db953
annamnshkv <anna.menshikova21@gmail.com>
2021-05-25 19:32:23 +0300

    share code to github

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <header className="row">
           <div>
             <a className="brand" href="/">
-              FoodClick
+              amazona
             </a>
           </div>
           <div>
